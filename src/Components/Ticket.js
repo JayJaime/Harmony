e8397bc00fcd796c76dad98df54401c5f7c4fc41
Austin Layman <61898015+EnkiEnki77@users.noreply.github.com>
2021-11-22 22:28:10 -0800

    working on ticket page

---
+++ src/Components/Ticket.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../Styles/Ticket.css'
+import DashButton from '../Components/DashButton'
+
+export default function Ticket(props) {
+    return (
+        <div className='ticket'>
+            <p></p>
+            <p></p>
+            <DashButton/>
+        </div>
+    )
+}
--- src/Pages/TicketPage.js
+++ src/Pages/TicketPage.js
@@ -1,9 +1,43 @@
 import React from 'react'
+import Header from '../Components/Header'
 import '../Styles/TicketPage.css'
+import Ticket from '../Components/Ticket'
 
 export default function TicketPage() {
+    let ticketPlaceholder = [
+        {seat:{section:'Terrace', seat:'20A', row:'9'},
+         price: '$125'
+    }
+    ]
+
     return (
-        <div>
+        <div className='ticketPageContainer'>
+            <Header/>
+            <div className='map'>
+                Stadium Map
+            </div>
+            <div className='ticketContentContainer'>
+                <div className='artistTicketPage'>
+                    <div className='tickArtistContent'>
+                        <div className='tickArtistDateLocation'>
+                            <div className='tickLocation'>
+                                <h3>Kendrick Lamar</h3>
+                                <p>Sunshine Theatre, Albuquerque, NM</p>
+                            </div>
+                            <div className='tickDate'>
+                                <p>Friday ⋅ Nov 12, 2022 ⋅ 8:00PM</p>
+                            </div>
+                        </div>
+                        <div className='tickArtistPhoto'></div>
+                    </div>
+                </div>
+                <div className='ticketAmtContainer'></div>
+                <div className='ticketContainer'>
+                    {ticketPlaceholder.map((ticket) => {
+                        return <Ticket price = {ticket.price} section = {ticket.seat.section} row = {ticket.seat.row} seat = {ticket.seat.seat}/>
+                    })}
+                </div>
+            </div>
             
         </div>
     )
