b0a4e690d6fdb7e0482a46dbb4712937bb25c480
TC <55402757+thory01@users.noreply.github.com>
2021-12-04 21:54:09 -0500

    latest updates

--- src/Components/config_example.js
+++ src/Components/config_example.js
@@ -2,7 +2,7 @@ export const authEndpoint = "https://accounts.spotify.com/authorize";
 
 // Replace with your app's client ID, redirect URI and desired scopes
 export const clientId = "bdf1368128624e7cb1c023357a4c1a61";
-export const redirectUri = "http://localhost:3000";
+export const redirectUri = "http://localhost:3000/Dashboard";
 export const scopes = [
 	"user-top-read",
 	"user-read-currently-playing",
--- src/Pages/Landing.js
+++ src/Pages/Landing.js
@@ -3,6 +3,9 @@ import Header from "../Components/Header";
 import styled from "styled-components";
 import myImage from "../images/Landing_Hero_Image_Edited.jpg";
 import { BsSpotify } from "react-icons/bs";
+import { useEffect, useState } from "react";
+import hash from "../Components/hash";
+import * as $ from "jquery";
 import {
 	authEndpoint,
 	clientId,
@@ -10,21 +13,51 @@ import {
 	scopes,
 } from "../Components/config_example";
 
-import { useEffect, useState } from "react";
-
 // function Login() {
 // 	window.open("Login", "_self");
 // }
 
 document.body.classList.add("overflow-y");
-
 export default function Landing() {
 	const [className, setClassName] = useState("");
+	const [token, setToken] = useState(null);
+
+	const getTopArtists = (token) => {
+		$.ajax({
+			url: "https://api.spotify.com/v1/me/top/artists",
+			type: "GET",
+			beforeSend: (xhr) => {
+				xhr.setRequestHeader("Authorization", "Bearer " + token);
+			},
+			success: (data) => {
+				// Checks if the data is not empty
+				// if (!data) {
+				// 	this.setState({
+				// 		no_data: true,
+				// 	});
+				// 	return;
+				// }
+				console.log(data);
+			},
+		});
+	};
+
+	useEffect(() => {
+		let _token = hash.access_token;
+
+		if (_token) {
+			// Set token
+			setToken(_token);
+
+			getTopArtists(_token);
+		}
+	}, []);
 
 	useEffect(() => {
 		setClassName("hidden");
 		return;
 	}, []);
+
 	return (
 		<Container>
 			<Header class={className} />
@@ -34,30 +67,27 @@ export default function Landing() {
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tellus leo
 					lectus mauris accumsan turpis.
 				</DashContent>
-
-				{!this.state.token && (
-					<a
-						className="btn btn--loginApp-link"
-						href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
-							"%20"
-						)}&response_type=token&show_dialog=true`}
-					>
-						<LandingButt>
+				<LandingButt>
+					{!token && (
+						<a
+							// className="btn btn--loginApp-link"
+							href={`${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
+								"%20"
+							)}&response_type=token&show_dialog=true`}
+						>
 							Sign in with Spotify <SpotifyIcon />{" "}
-						</LandingButt>
-					</a>
-				)}
+						</a>
+					)}
+				</LandingButt>
 			</ImageContainer>
 		</Container>
 	);
 }
-
-let Container = styled.div`
+const Container = styled.div`
 	height: 100vh;
 	width: 100vw;
 `;
-
-let ImageContainer = styled.div`
+const ImageContainer = styled.div`
 	box-sizing: border-box;
 	background-image: url(${myImage});
 	background-size: cover;
@@ -65,7 +95,6 @@ let ImageContainer = styled.div`
 	height: 100%;
 	background-repeat: no-repeat;
 `;
-
 const DashTitle = styled.h2`
 	position: absolute;
 	width: 590px;
@@ -73,7 +102,6 @@ const DashTitle = styled.h2`
 	left: 50%;
 	transform: translateX(-50%);
 	top: 148px;
-
 	font-family: Montserrat;
 	font-style: normal;
 	font-weight: bold;
@@ -81,10 +109,8 @@ const DashTitle = styled.h2`
 	line-height: 73px;
 	text-align: center;
 	letter-spacing: 0.01em;
-
 	color: #ffffff;
 `;
-
 const DashContent = styled.p`
 	position: absolute;
 	width: 590px;
@@ -92,34 +118,29 @@ const DashContent = styled.p`
 	left: 50%;
 	transform: translateX(-50%);
 	top: 310px;
-
 	font-family: Montserrat;
 	font-style: normal;
 	font-weight: 500;
 	font-size: 18px;
 	line-height: 30px;
 	/* or 167% */
-
 	text-align: center;
 	letter-spacing: 0.02em;
-
 	color: #ffffff;
 `;
-
 const LandingButt = styled.button`
 	display: flex;
 	flex-direction: row;
 	justify-content: center;
 	align-items: center;
-	padding: 26px 10px;
-
+	padding: 16px 24px;
+	font-family: "Montserrat";
 	position: absolute;
-	width: 241px;
+	/* width: 241px; */
 	height: 52px;
 	left: 50%;
 	transform: translateX(-50%);
 	top: 418px;
-
 	border: 2px solid #1db954;
 	box-sizing: border-box;
 	background: none;
@@ -128,17 +149,14 @@ const LandingButt = styled.button`
 	font-size: 18px;
 	letter-spacing: 1px;
 	/* light-shadow */
-
 	filter: drop-shadow(0px 4px 32px rgba(60, 69, 76, 0.12));
 	border-radius: 30px;
-
 	&:hover {
 		background-color: #1db954;
 		color: #000000;
 		cursor: pointer;
 	}
 `;
-
 const SpotifyIcon = styled(BsSpotify)`
 	transform: translateX(5px);
 	font-size: 20px;
